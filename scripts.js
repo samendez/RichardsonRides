bbc6a293f1d46ac990e53647dc5542f1053d51e8
jaymspeights <jaymspeights@gmail.com>
2016-09-29 21:27:11 -0500

    Allows position tracking via tag title attribute update. tested on PC in chrome

---
+++ scripts.js
@@ -0,0 +1,19 @@
+function updateLocation() {
+ if (navigator && navigator.geolocation) {
+            return navigator.geolocation.getCurrentPosition(successCallback, errorCallback, {maximumAge: 30000});
+        } else {
+            console.log('Geolocation is not supported');
+        }
+
+}
+
+function errorCallback(){
+  console.log('Geolocation error');
+}
+
+function successCallback(position) {
+  var lon = position.coords.longitude;
+  var lat = position.coords.latitude;
+  document.getElementById("position").title = lat + ", " + lon;
+
+}
