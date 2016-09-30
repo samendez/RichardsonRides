cf8cab8d43804f33f652cdf395d7aa84883775c8
jaymspeights <jaymspeights@gmail.com>
2016-09-30 01:13:22 -0500

    Added index.html first draft, functioning positioning, and updated scripts

--- scripts.js
+++ scripts.js
@@ -1,19 +1,33 @@
+//var position;
+
 function updateLocation() {
- if (navigator && navigator.geolocation) {
-            return navigator.geolocation.getCurrentPosition(successCallback, errorCallback, {maximumAge: 30000});
-        } else {
-            console.log('Geolocation is not supported');
-        }
+    if (navigator && navigator.geolocation) {
+        return navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    } else {
+        console.log('Geolocation is not supported');
+    }
 
 }
 
-function errorCallback(){
-  console.log('Geolocation error');
+function errorCallback() {
+    console.log('Geolocation error');
 }
 
 function successCallback(position) {
-  var lon = position.coords.longitude;
-  var lat = position.coords.latitude;
-  document.getElementById("position").title = lat + ", " + lon;
+    var lon = position.coords.longitude;
+    var lat = position.coords.latitude;
+    // position = lat + ", " + lon;
+    console.log(lat + ", " + lon);
+    document.getElementById("pos").value = lat + ", " + lon;
 
+}
+
+function getPosition() {
+    //
+    // return position;
+}
+
+function setPosition() {
+    updateLocation();
+    // document.getElementById("pos").value = getPosition();
 }
